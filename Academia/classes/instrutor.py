222077d66416ad9b9a406b467797dccb74988fad
davi-scg <davi.gomes2@estudante.ifto.edu.br>
2024-05-04 12:10:47 -0300

    Criação da Academia

---
+++ Academia/classes/instrutor.py
@@ -0,0 +1,10 @@
+class Instrutor:
+
+    def __init__(self, nome, sobrenome, data_nascimento, endereco, telefone, email, id = None):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.data_nascimento = data_nascimento
+        self.endereco = endereco
+        self.telefone = telefone
+        self.email = email
+        self.id = id
